cb3ba9bec26f2bce3cb9a50107d531d43961fb28
nineOneOnePawel <p_gajewski@hotmail.com>
2019-01-15 15:18:59 +0000

    coming soon

---
+++ main.js
@@ -0,0 +1,40 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 3, 2019 08:15:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="timeCountdown"
+  document.getElementById("timeCountdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timeCountdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+var i = 0;
+var txt = 'Welcome to AssemblyNest! Coming soon...'; /* The text */
+var speed = 80; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
