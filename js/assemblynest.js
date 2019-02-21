bfe0a5175f1e6bd2927c92bb33a07ba56bfc164c
nineoneonepawel <p_gajewski@hotmail.com>
2019-02-21 21:09:12 +0000

    added character counter textarea

--- js/assemblynest.js
+++ js/assemblynest.js
@@ -1,3 +1,15 @@
 function enableSubmitBtn(){
     document.getElementById("submitBtn").disabled = false;
 }
+
+// Counting textarea input id=charNum
+function countChar(val) {
+    var max = 400;
+    var len = val.value.length;
+    if (len >= max) {
+      $('#charNum').text(' you have reached the limit');
+    } else {
+      var char = max - len;
+      $('#charNum').text(len+'/'+char);
+    }
+  }
